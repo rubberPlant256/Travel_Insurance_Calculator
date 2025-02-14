c19797e132b0c47ac8d088eed778e99a8fcb1346
rubberPlant256 <ihrustal@yandex.ru>
2025-02-14 12:57:09 +0300

    STEP_012

--- internship/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumResponse.java
+++ internship/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumResponse.java
@@ -9,4 +9,45 @@ public class TravelCalculatePremiumResponse {
     private Date agreementDateFrom;
     private Date agreementDateTo;
 
+    public TravelCalculatePremiumResponse(){}
+
+    public TravelCalculatePremiumResponse(String personFirstName, String personLastName,
+                                         Date agreementDateFrom, Date agreementDateTo){
+        this.personFirstName = personFirstName;
+        this.personLastName = personLastName;
+        this.agreementDateFrom = agreementDateFrom;
+        this.agreementDateTo = agreementDateTo;
+    }
+
+    public String getPersonFirstName() {
+        return this.personFirstName;
+    }
+
+    public String getPersonLastName() {
+        return this.personLastName;
+    }
+
+    public Date getAgreementDateFrom() {
+        return this.agreementDateFrom;
+    }
+
+    public Date getAgreementDateTo() {
+        return this.agreementDateTo;
+    }
+
+    public void setPersonFirstName(String personFirstName) {
+        this.personFirstName = personFirstName;
+    }
+
+    public void setPersonLastName(String personLastName) {
+        this.personLastName = personLastName;
+    }
+
+    public void setAgreementDateFrom(Date agreementDateFrom) {
+        this.agreementDateFrom = agreementDateFrom;
+    }
+
+    public void setAgreementDateTo(Date agreementDateTo) {
+        this.agreementDateTo = agreementDateTo;
+    }
 }
